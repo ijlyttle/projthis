fe9534a88ee9b3ea5271cfae93ad6cfb1ac48d8b
Ian Lyttle <ian.lyttle@schneider-electric.com>
2020-10-24 18:39:26 -0500

    add proj_create(), document

---
+++ R/proj_create.R
@@ -0,0 +1,31 @@
+#' Create project
+#'
+#' To create a new project, this function calls [usethis::create_project()],
+#' then [proj_use_description()] to create `DESCRIPTION` file.
+#'
+#' @inheritParams usethis::create_project
+#' @inheritParams proj_use_description
+#'
+#' @return Invisible `NULL`, called for side effects
+#' @examples
+#' # not run because it evokes side effects
+#' if (FALSE) {
+#'   proj_create("../new-project-name")
+#' }
+#' @export
+#'
+proj_create <- function(path, rstudio = rstudioapi::isAvailable(),
+                        open = rlang::is_interactive(), fields = list()) {
+
+  # create project
+  usethis::create_project(path = path, rstudio = rstudio, open = open)
+
+  # add DESCRIPTION file
+  proj_use_description(fields = fields, update_deps = TRUE)
+
+  # Give feedback
+  code <- usethis::ui_code("proj_update_deps()")
+  pui_todo("As you add dependencies, call {code} to update DESCRIPTION")
+
+  invisible(NULL)
+}
