d1414d33221cbc33e919f790af4b9dc40dcbdc06
Ian Lyttle <ian.lyttle@schneider-electric.com>
2021-03-25 14:59:16 -0500

    update tests, bump version

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -16,6 +16,19 @@ test_that("sort_files works", {
      sort_files(tolower(files_sorted), first = NULL, last = NULL),
      tolower(files_sorted)
    )
+
+   files_sorted_zoom_first <- c("zoom.Rmd", "00-import.Rmd", "01-clean.Rmd", "README.Rmd")
+   expect_identical(
+      sort_files(files_sorted, first = "zoom.Rmd", last = NULL),
+      files_sorted_zoom_first
+   )
+
+   files_sorted_00_last <- c("01-clean.Rmd", "zoom.Rmd", "00-import.Rmd", "README.Rmd")
+   expect_identical(
+      sort_files(files_sorted, first = NULL, last = "00-import.Rmd"),
+      files_sorted_00_last
+   )
+
 })
 
 test_that("print utilities work", {
